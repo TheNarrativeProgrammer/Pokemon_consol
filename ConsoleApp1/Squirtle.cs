fc1dfaf0e249fb44a52fdd5a447ffbf9a291bd7f
reidmgillis <reid.m.gillis@gmail.com>
2024-10-13 19:23:23 -0700

    ADDED Squirtle child class of Pokemon

--- ConsoleApp1/Squirtle.cs
+++ ConsoleApp1/Squirtle.cs
@@ -8,39 +8,24 @@ namespace ConsoleApp1
 {
     internal class Squirtle : Pokemon
     {
-
         //**CONSTRUCTORS**
-        public Squirtle()               //default constructor
-        {
-            //initialize member variables
-            this.mHP = 30;                          //Name and Health
-            this.mName = "Squirtle";
-            this.mLevel = 5;                        //Level and Experience Points
-            this.mEXPoints = 0;
-            this.mType = Type.Water;            //Type and weakenss
-            this.mWeakness = Weakness.Grass;
-            this.mAttack_Damage = 7;                //Attacks   
-            this.mAttack_AccuracyDemoninator = 10;
-            this.mAttack_BuffMultiplier = 1;
-            this.mAttack_WeaknessMultiplier = 1;
-            this.mAttack_StrengthSubtractor = 0;
-        }
 
-        public Squirtle(string InName)               //param constructor
+        public Squirtle(string InName,string InPokeName)                                                   //default constructor
         {
             //initialize member variables
-            this.mHP = 30;                          //Name and Health
+            this.mHP = 50;                          //Name and Health
             this.mName = InName;
+            this.mPokeName = InPokeName;
             this.mLevel = 5;                        //Level and Experience Points
             this.mEXPoints = 0;
             this.mType = Type.Water;            //Type and weakenss
             this.mWeakness = Weakness.Grass;
-            this.mAttack_Damage = 7;                //Attacks   
+            this.mAttack_Damage = 5;                //Attacks   
             this.mAttack_AccuracyDemoninator = 10;
             this.mAttack_BuffMultiplier = 1;
             this.mAttack_WeaknessMultiplier = 1;
             this.mAttack_StrengthSubtractor = 0;
+            this.mDidAttackLand = true;
         }
-
     }
 }
